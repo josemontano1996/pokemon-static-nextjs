052026c822fcadac56bc0c2296e76a7a628258c8
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-07-31 16:12:10 +0200

    first commit

---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export const MainLayout = () => {
+  return (
+    <>
+      <Head>
+        <title>Pokemon App</title>
+        <meta name='author' content='Jose Manuel Montano Mengual' />
+        <meta name='description' content='Pokemon about XXXX pokemon' />
+        <meta name='keywords' content=' XXXX, pokemon, pokedex' />
+      </Head>
+      <main></main>
+    </>
+  );
+};
