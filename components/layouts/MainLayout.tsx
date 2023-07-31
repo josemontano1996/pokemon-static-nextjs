a4afd9778b1bcdb12d7cc48fb31c5ec9dd112cd4
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-07-31 17:25:46 +0200

    adding navbar;

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -1,15 +1,25 @@
+import { FC, ReactNode } from 'react';
 import Head from 'next/head';
+import { NavBar } from '../ui';
 
-export const MainLayout = () => {
+interface LayoutProps {
+  children: ReactNode;
+  title?: string;
+}
+
+export const MainLayout: FC<LayoutProps> = ({ children, title }) => {
   return (
     <>
       <Head>
-        <title>Pokemon App</title>
+        <title>{title || 'Pokemon App'}</title>
         <meta name='author' content='Jose Manuel Montano Mengual' />
-        <meta name='description' content='Pokemon about XXXX pokemon' />
-        <meta name='keywords' content=' XXXX, pokemon, pokedex' />
+        <meta name='description' content={`Pokemon about ${title}`} />
+        <meta name='keywords' content={`${title}, pokemon, pokedex`} />
       </Head>
-      <main></main>
+
+      <NavBar />
+
+      <main style={{ padding: '0px 20px' }}>{children}</main>
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import { Button } from '@nextui-org/react';
 import { NextPage } from 'next';
+import { MainLayout } from '../components/layouts';
 
 const HomePage: NextPage = () => {
   return (
     <>
-      <h1>Hola Mundo</h1>
-      <Button color='gradient'>Hola Mundo</Button>
+      <MainLayout title='Pokemon List'>
+        <h1>Hola Mundo</h1>
+        <Button color='gradient'>Hola Mundo</Button>
+      </MainLayout>
     </>
   );
 };
