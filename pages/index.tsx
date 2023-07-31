f1962b5a682da09ee11d81c62c4f12ba9f615eb5
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-07-31 19:11:48 +0200

    added first ssg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,43 @@
-import { Button } from '@nextui-org/react';
-import { NextPage } from 'next';
+import { GetStaticProps, NextPage } from 'next';
 import { MainLayout } from '../components/layouts';
+import { pokeApi } from '@/api';
+import { PokemonListResponse, SmallPokemon } from '@/interfaces';
 
-const HomePage: NextPage = () => {
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+const HomePage: NextPage<Props> = ({ pokemons }) => {
   return (
     <>
       <MainLayout title='Pokemon List'>
-        <h1>Hola Mundo</h1>
-        <Button color='gradient'>Hola Mundo</Button>
+        <ul>
+          {pokemons.map((pokemon) => (
+            <li key={pokemon.id}>
+              {pokemon.name}, {pokemon.id}
+            </li>
+          ))}
+        </ul>
       </MainLayout>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
+
+  const pokemons:SmallPokemon[] = data.results.map((pokemon, index) => {
+    const id = index + 1;
+    pokemon.id = id;
+    pokemon.img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
+    return pokemon;
+  });
+
+  return {
+    props: {
+      pokemons,
+    },
+  };
 };
 
 export default HomePage;
