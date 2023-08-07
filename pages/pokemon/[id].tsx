2cff22b1fafcbe1e3d6126571a953b40b6c79b85
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-08-07 09:41:06 +0200

    added isg

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -105,17 +105,30 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 
   return {
     paths,
-    fallback: false,
+    /* fallback: false, */
+    fallback: 'blocking',
   };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string };
 
+  const pokemon = await getPokemonInfo(id);
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
   return {
     props: {
-      pokemon: await getPokemonInfo(id),
+      pokemon,
     },
+    revalidate: 86400, //time in secs
   };
 };
 
